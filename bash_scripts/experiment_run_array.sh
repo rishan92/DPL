9f7c276d16dfdabcaaa18b76c9d4dfa383b8e3a8
Rishan <rd.senanayake@gmail.com>
2023-04-05 10:28:29 +0200

    Change nemo batch script files

--- bash_scripts/experiment_run_array.sh
+++ bash_scripts/experiment_run_array.sh
@@ -4,18 +4,19 @@ export benchmark=$1
 
 if [ "$benchmark" == "lcbench" ]
 then
-  file="lcbench_dataset_names.txt"
+  file="$ROOT_PATH/bash_scripts/lcbench_dataset_names.txt"
 elif [ "$benchmark" == "taskset" ]
 then
-  file="taskset_dataset_names.txt"
+  file="$ROOT_PATH/bash_scripts/taskset_dataset_names.txt"
 elif [ "$benchmark" == "nasbench201" ]
 then
-  file="nasbench_dataset_names.txt"
+  file="$ROOT_PATH/bash_scripts/nasbench_dataset_names.txt"
 else
-  file="pd1_dataset_names.txt"
+  file="$ROOT_PATH/bash_scripts/pd1_dataset_names.txt"
 fi
 
-export dir="$2"
+export surrogate="$2"
+export dir="$3"
 
 if ! [ -e "$file" ] ; then     # spaces inside square brackets
     echo "$0: $file does not exist" >&2  # error message includes $0 and goes to stderr
@@ -26,5 +27,5 @@ NAMES=$(<$file)
 for NAME in $NAMES
 do
    export dataset=$(echo $NAME)
-   msub -V -t 1-10 experiment_array.moab
+   msub -V -t 1-10 "$ROOT_PATH"/bash_scripts/experiment_array.moab
 done
--- bash_scripts/submit.sh
+++ bash_scripts/submit.sh
@@ -1,2 +1,18 @@
-./experiment_run_array.sh "taskset" "/work/ws/nemo/fr_ak1206-learning_curve-0/power_law"
+#!/bin/bash
 
+export ROOT_PATH=/home/fr/fr_fr/fr_rs442/project/DPL
+export ENV_PATH=/home/fr/fr_fr/fr_rs442/miniconda3/envs/dpl
+
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "taskset" "power_law" ""$ROOT_PATH"/results"
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "lcbench" "power_law" ""$ROOT_PATH"/results"
+
+
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "taskset" "random" "$ROOT_PATH/results"
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "taskset" "asha" "$ROOT_PATH/results"
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "taskset" "dehb" "$ROOT_PATH/results"
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "taskset" "dragonfly" ""$ROOT_PATH"/results"
+
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "lcbench" "random" ""$ROOT_PATH"/results"
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "lcbench" "asha" ""$ROOT_PATH"/results"
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "lcbench" "dehb" ""$ROOT_PATH"/results"
+# "$ROOT_PATH"/bash_scripts/experiment_run_array.sh "lcbench" "dragonfly" ""$ROOT_PATH"/results"
