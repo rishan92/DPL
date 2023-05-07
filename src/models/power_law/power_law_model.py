1c1dbe33cb600d1a4d5020371439fe8162aed6d1
Rishan <rd.senanayake@gmail.com>
2023-05-07 13:25:09 +0200

    lower lr finder diverge threshold

--- src/models/power_law/power_law_model.py
+++ src/models/power_law/power_law_model.py
@@ -323,7 +323,7 @@ class PowerLawModel(BasePytorchModule, ABC):
         model.hook_remove()
         model.set_optimizer(use_scheduler=False)
         lr_finder = LRFinder(model=model, optimizer=model.optimizer, criterion=model.criterion, device=model_device)
-        lr_finder.range_test(train_dataloader, start_lr=1e-8, end_lr=1, num_iter=100, diverge_th=2)
+        lr_finder.range_test(train_dataloader, start_lr=1e-8, end_lr=1, num_iter=100, diverge_th=1.1)
         # lr_finder.plot()
         suggested_lr = lr_finder.get_suggested_lr()
         print(f"suggested_lr {suggested_lr}")
