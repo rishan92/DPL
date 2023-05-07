fix lr finder when gradients become nan

src/models/power_law/complex4_power_law_model.py
@@ -27,7 +27,11 @@ class Complex4PowerLawModel(PowerLawModel):
             'learning_rate': 1e-3,
             'act_func': 'LeakyReLU',
             'clip_gradients': 0,
-            'last_act_func': 'SelfGLU',
+            'last_act_func': 'Identity',
+            'alpha_act_func': 'Identity',
+            'beta_act_func': 'Identity',
+            'gamma_act_func': 'Identity',
+            'gammai_act_func': 'Identity',
             'output_act_func': None,
             'loss_function': 'L1Loss',
             'optimizer': 'Adam',
@@ -87,10 +91,10 @@ class Complex4PowerLawModel(PowerLawModel):
         gammas_r_b = x[:, 2]
         gammas_i_b = x[:, 3]
 
-        alphas_r = alphas_r_b
-        betas_r = self.last_act_func(betas_r_b)
-        gammas_r = self.last_act_func(gammas_r_b)
-        gammas_i = self.last_act_func(gammas_i_b)
+        alphas_r = self.alpha_act_func(alphas_r_b)
+        betas_r = self.beta_act_func(betas_r_b)
+        gammas_r = self.gamma_act_func(gammas_r_b)
+        gammas_i = self.gammai_act_func(gammas_i_b)
 
         b = torch.tensor(1 / 51)
         alphas_i = betas_r * torch.sin(-1 * b * gammas_i) / (torch.cos(-1 * b * gammas_i) - torch.exp(b * gammas_r))

src/models/power_law/conditioned_power_law_model.py
@@ -22,11 +22,14 @@ class ConditionedPowerLawModel(PowerLawModel):
             'nr_cnn_layers': 2,
             'use_learning_curve': False,
             'use_learning_curve_mask': False,
-            'use_suggested_learning_rate': False,
+            'use_suggested_learning_rate': True,
             'learning_rate': 1e-3,
             'refine_learning_rate': 1e-3,
             'act_func': 'LeakyReLU',
-            'last_act_func': 'SelfGLU',
+            'last_act_func': 'Identity',
+            'alpha_act_func': 'Identity',
+            'beta_act_func': 'SelfGLU',
+            'gamma_act_func': 'SelfGLU',
             'output_act_func': None,
             'loss_function': 'L1Loss',
             'optimizer': 'Adam',
@@ -116,13 +119,17 @@ class ConditionedPowerLawModel(PowerLawModel):
         betas = x[:, 1]
         gammas = x[:, 2]
 
+        alphas = self.alpha_act_func(alphas)
+        betas = self.beta_act_func(betas)
+        gammas = self.gamma_act_func(gammas)
+
         output = torch.add(
             alphas,
             torch.mul(
-                self.last_act_func(betas),
+                betas,
                 torch.pow(
                     predict_budgets,
-                    torch.mul(self.last_act_func(gammas), -1)
+                    torch.mul(gammas, -1)
                 )
             ),
         )

src/models/power_law/power_law_model.py
@@ -62,6 +62,9 @@ class PowerLawModel(BasePytorchModule, ABC):
         self.alpha_act_func = None
         self.beta_act_func = None
         self.gamma_act_func = None
+        self.alphai_act_func = None
+        self.betai_act_func = None
+        self.gammai_act_func = None
         self.output_act_func = None
         self.output_act_inverse_func = None
         self.linear_net = None
@@ -92,6 +95,16 @@ class PowerLawModel(BasePytorchModule, ABC):
             # a = self.output_act_func(torch.tensor(val))
             # b = self.output_act_inverse_func(a)
 
+        if hasattr(self.meta, "alphai_act_func"):
+            self.alphai_act_func = get_class_from_packages([torch.nn, src.models.activation_functions],
+                                                           self.meta.alphai_act_func)()
+        if hasattr(self.meta, "betai_act_func"):
+            self.betai_act_func = get_class_from_packages([torch.nn, src.models.activation_functions],
+                                                          self.meta.betai_act_func)()
+        if hasattr(self.meta, "gammai_act_func"):
+            self.gammai_act_func = get_class_from_packages([torch.nn, src.models.activation_functions],
+                                                           self.meta.gammai_act_func)()
+
         self.linear_net = self.get_linear_net()
 
         if hasattr(self.meta, "use_learning_curve") and self.meta.use_learning_curve:
@@ -102,6 +115,8 @@ class PowerLawModel(BasePytorchModule, ABC):
         self.has_batchnorm_layers = False
         self.optimizer = None
         self.lr_scheduler = None
+
+        self.hook_handle = None
 
         self.clip_gradients_value = None
         if hasattr(self.meta, 'clip_gradients') and self.meta.clip_gradients != 0:
@@ -117,7 +132,7 @@ class PowerLawModel(BasePytorchModule, ABC):
             if hasattr(self, 'param_names'):
                 hook = partial(self.gradient_logging_hook, names=self.param_names)
                 if hasattr(self, 'linear_net') and self.linear_net is not None:
-                    self.linear_net.register_full_backward_hook(hook=hook)
+                    self.hook_handle = self.linear_net.register_full_backward_hook(hook=hook)
                 else:
                     warnings.warn("Gradient flow tracking with wandb is not supported for this module.")
 
@@ -305,11 +320,13 @@ class PowerLawModel(BasePytorchModule, ABC):
         model = deepcopy(self)
         # Change instance id from zero to stop recursive calls.
         model.instance_id = 1000
+        model.hook_remove()
         model.set_optimizer(use_scheduler=False)
         lr_finder = LRFinder(model=model, optimizer=model.optimizer, criterion=model.criterion, device=model_device)
         lr_finder.range_test(train_dataloader, start_lr=1e-8, end_lr=1, num_iter=100, diverge_th=2)
         # lr_finder.plot()
         suggested_lr = lr_finder.get_suggested_lr()
+        print(f"suggested_lr {suggested_lr}")
         return suggested_lr
 
     def __getstate__(self):
@@ -324,6 +341,10 @@ class PowerLawModel(BasePytorchModule, ABC):
         self.__dict__.update(state)
         # Restore unpickable objects from the state dictionary
         self.logger = logger
+
+    def hook_remove(self):
+        if self.hook_handle:
+            self.hook_handle.remove()
 
     def reset(self):
         PowerLawModel._instance_counter = 0

src/utils/torch_lr_finder.py
@@ -6,6 +6,7 @@ from tqdm.autonotebook import tqdm
 from torch.optim.lr_scheduler import _LRScheduler
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+from pathlib import Path
 
 from packaging import version
 
@@ -236,30 +237,30 @@ class LRFinder(object):
                 e.g., moving CPU Tensors with pinned memory to CUDA devices. Default: True.
 
         Example (fastai approach):
-            >>> lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
-            >>> lr_finder.range_test(dataloader, end_lr=100, num_iter=100)
+            # >>> lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
+            # >>> lr_finder.range_test(dataloader, end_lr=100, num_iter=100)
 
         Example (Leslie Smith's approach):
-            >>> lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
-            >>> lr_finder.range_test(trainloader, val_loader=val_loader, end_lr=1, num_iter=100, step_mode="linear")
+            # >>> lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
+            # >>> lr_finder.range_test(trainloader, val_loader=val_loader, end_lr=1, num_iter=100, step_mode="linear")
 
         Gradient accumulation is supported; example:
-            >>> train_data = ...    # prepared dataset
-            >>> desired_bs, real_bs = 32, 4         # batch size
-            >>> accumulation_steps = desired_bs // real_bs     # required steps for accumulation
-            >>> dataloader = torch.utils.data.DataLoader(train_data, batch_size=real_bs, shuffle=True)
-            >>> acc_lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
-            >>> acc_lr_finder.range_test(dataloader, end_lr=10, num_iter=100, accumulation_steps=accumulation_steps)
+            # >>> train_data = ...    # prepared dataset
+            # >>> desired_bs, real_bs = 32, 4         # batch size
+            # >>> accumulation_steps = desired_bs // real_bs     # required steps for accumulation
+            # >>> dataloader = torch.utils.data.DataLoader(train_data, batch_size=real_bs, shuffle=True)
+            # >>> acc_lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
+            # >>> acc_lr_finder.range_test(dataloader, end_lr=10, num_iter=100, accumulation_steps=accumulation_steps)
 
         If your DataLoader returns e.g. dict, or other non standard output, intehit from TrainDataLoaderIter,
         redefine method `inputs_labels_from_batch` so that it outputs (inputs, lables) data:
-            >>> import torch_lr_finder
-            >>> class TrainIter(torch_lr_finder.TrainDataLoaderIter):
-            >>>     def inputs_labels_from_batch(self, batch_data):
-            >>>         return (batch_data['user_features'], batch_data['user_history']), batch_data['y_labels']
-            >>> train_data_iter = TrainIter(train_dl)
-            >>> finder = torch_lr_finder.LRFinder(model, optimizer, partial(model._train_loss, need_one_hot=False))
-            >>> finder.range_test(train_data_iter, end_lr=10, num_iter=300, diverge_th=10)
+            # >>> import torch_lr_finder
+            # >>> class TrainIter(torch_lr_finder.TrainDataLoaderIter):
+            # >>>     def inputs_labels_from_batch(self, batch_data):
+            # >>>         return (batch_data['user_features'], batch_data['user_history']), batch_data['y_labels']
+            # >>> train_data_iter = TrainIter(train_dl)
+            # >>> finder = torch_lr_finder.LRFinder(model, optimizer, partial(model._train_loss, need_one_hot=False))
+            # >>> finder.range_test(train_data_iter, end_lr=10, num_iter=300, diverge_th=10)
 
         Reference:
         [Training Neural Nets on Larger Batches: Practical Tips for 1-GPU, Multi-GPU & Distributed setups](
@@ -371,6 +372,7 @@ class LRFinder(object):
     def _train_batch(self, train_iter, accumulation_steps, non_blocking_transfer=True):
         self.model.train()
         total_loss = None  # for late initialization
+        is_nan_gradient = False
 
         self.optimizer.zero_grad()
         for i in range(accumulation_steps):
@@ -399,14 +401,25 @@ class LRFinder(object):
             else:
                 loss.backward()
 
+            if not is_nan_gradient:
+                for name, param in self.model.named_parameters():
+                    if param.grad is not None:
+                        if torch.isnan(param.grad).any():
+                            is_nan_gradient = True
+                            break
+
+            loss_value = loss.item()
+            if is_nan_gradient:
+                loss_value = 1000
+
             if total_loss is None:
-                total_loss = loss
+                total_loss = loss_value
             else:
-                total_loss += loss
+                total_loss += loss_value
 
         self.optimizer.step()
 
-        return total_loss.item()
+        return total_loss
 
     def _move_to_device(self, inputs, labels, non_blocking=True):
         def move(obj, device, non_blocking=True):
@@ -541,7 +554,7 @@ class LRFinder(object):
                 suggested_value = lrs[min_grad_idx]
             fig.suptitle(f"Suggested LR: {suggested_value:.2E}")
             # plt.show()
-            file_path = f"SuggestLearningRate_id_{LRFinder._calculation_id}"
+            file_path = Path(f"SuggestLearningRate_id_{LRFinder._calculation_id}")
             plt.savefig(file_path, dpi=70)
 
         if suggest_lr and min_grad_idx is not None: