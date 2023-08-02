d758c24dbb8523f193c49b4bbbf000ecdeb43b96
Rishan <rd.senanayake@gmail.com>
2023-08-02 16:13:21 +0200

    small fix

--- global_variables.py
+++ global_variables.py
@@ -3,7 +3,7 @@ import os
 
 user_name = getpass.getuser()
 
-IS_NEMO = 1 if user_name == 'fr_rs442' else 0
+IS_NEMO = 1 if user_name == 'fr_rs442' or user_name == 'senanaya' else 0
 
 IS_WANDB = 1
 if IS_WANDB and "WANDB_API_KEY" not in os.environ:
